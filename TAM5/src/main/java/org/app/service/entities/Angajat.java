dff40f4cf9610b671201bf2e55f27d07bac8b559
Veronica <Veronica@DESKTOP-OSB4J7N>
2018-11-07 00:26:17 +0200

    jpa

--- TAM5/src/main/java/org/app/service/entities/Angajat.java
+++ TAM5/src/main/java/org/app/service/entities/Angajat.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 @Entity
 public class Angajat {
@@ -20,116 +21,80 @@ String numePrenume;
 String adresa;
 Integer telefon;
 Double salariu;
+@ManyToOne
 Departament departament;
-
 @OneToMany
 List<Asigurare> asigurari = new ArrayList<Asigurare>();
 @OneToMany
 List<Departament> departamente = new ArrayList<Departament>();
 @OneToMany
 List<TestAngajat> testeAngajati = new ArrayList<TestAngajat>();
-
 public Integer getMarcaAngajat() {
 	return marcaAngajat;
 }
 public void setMarcaAngajat(Integer marcaAngajat) {
 	this.marcaAngajat = marcaAngajat;
 }
-
 public Integer getNrContract() {
 	return nrContract;
 }
-
 public void setNrContract(Integer nrContract) {
 	this.nrContract = nrContract;
 }
-
 public Integer getCNP() {
 	return CNP;
 }
-
 public void setCNP(Integer cNP) {
 	CNP = cNP;
 }
-
 public Integer getNumarCI() {
 	return numarCI;
 }
-
 public void setNumarCI(Integer numarCI) {
 	this.numarCI = numarCI;
 }
-
 public String getSerieCI() {
 	return serieCI;
 }
-
 public void setSerieCI(String serieCI) {
 	this.serieCI = serieCI;
 }
-
 public String getStare() {
 	return stare;
 }
-
 public void setStare(String stare) {
 	this.stare = stare;
 }
-
 public String getNumePrenume() {
 	return numePrenume;
 }
-
 public void setNumePrenume(String numePrenume) {
 	this.numePrenume = numePrenume;
 }
-
 public String getAdresa() {
 	return adresa;
 }
-
 public void setAdresa(String adresa) {
 	this.adresa = adresa;
 }
-
 public Integer getTelefon() {
 	return telefon;
 }
-
 public void setTelefon(Integer telefon) {
 	this.telefon = telefon;
 }
-
 public Double getSalariu() {
 	return salariu;
 }
-
 public void setSalariu(Double salariu) {
 	this.salariu = salariu;
 }
-
-public Integer getIdPost() {
-	return idPost;
+public Departament getDepartament() {
+	return departament;
+}
+public void setDepartament(Departament departament) {
+	this.departament = departament;
 }
 
-public void setIdPost(Integer idPost) {
-	this.idPost = idPost;
-}
-
-public String getIdDepartament() {
-	return idDepartament;
-}
-
-public void setIdDepartament(String idDepartament) {
-	this.idDepartament = idDepartament;
-}
-
-public List<Asigurare> getAsigurari() {
-	return asigurari;
-}
-
-public void setAsigurari(List<Asigurare> asigurari) {
-	this.asigurari = asigurari;
-}
 
 }
